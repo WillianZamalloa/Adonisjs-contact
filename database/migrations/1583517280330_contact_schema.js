d55e1e7992b5220db8efc98920552089895b9ee9
Willian Zamalloa Paro <wzamalloa@hotmail.com>
2022-11-24 18:17:31 -0500

    Contactos en adonisjs 4.1

---
+++ database/migrations/1583517280330_contact_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ContactSchema extends Schema {
+  up () {
+    this.create('contacts', (table) => {
+      table.increments()
+      table.string('nombre').notNullable()
+      table.string('telefono').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('contacts')
+  }
+}
+
+module.exports = ContactSchema
